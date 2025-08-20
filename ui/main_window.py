4ffd668311cee21a0f247d45f917cc0bb3a8bc82
Ron Moshailov <ronik2014@walla.com>
2025-08-20 22:16:24 +0300

    refactoring

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QPushButton, QHBoxLayout, QMainWindow, QLineEdit, QWidget, QGridLayout, QScrollArea
+from PyQt6.QtWidgets import QPushButton, QHBoxLayout, QMainWindow, QLineEdit, QWidget, QGridLayout, QScrollArea, \
+    QSpacerItem, QSizePolicy
 
 from config.special import set_blue_button_white_text_style, make_checkable, set_btn_disable
 from config.debug import displayAllMoves
@@ -19,13 +20,9 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(root)
         root_layout = QHBoxLayout(root)
 
-        # --- left panel placeholder ---
-        left_panel = QWidget(root)
-        # left_panel.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-
         # --- right panel with grid (scrollable) ---
         btn_container = QWidget(root)
-        grid = QGridLayout(btn_container)
+        btn_grid = QGridLayout(btn_container)
 
         # components
         name_edit = QLineEdit()
@@ -41,19 +38,27 @@ class MainWindow(QMainWindow):
                     QPushButton("הגדר מעברים")  ,  QPushButton("הדפס הכל"),
         ]
 
-        buttons[1]    .clicked.connect(lambda: self.data_controller.initialize_app([buttons[0]] + buttons[2:]))
-        buttons[3]   .clicked.connect(lambda: self.ui_controller.show_set_move_layout())
-        buttons[5]   .clicked.connect(lambda: self.ui_controller.show_min_green_layout())
-        buttons[7]   .clicked.connect(lambda: self.ui_controller.show_matrix_layout())
-        buttons[9]   .clicked.connect(lambda: self.ui_controller.show_sk_layout())
-        buttons[13]   .clicked.connect(lambda: displayAllMoves())
+      # buttons[0]       checkAble
+        buttons[1]      .clicked.connect(lambda: self.data_controller.initialize_app([buttons[0]] + buttons[2:]))
+      # buttons[2]       checkAble
+        buttons[3]      .clicked.connect(lambda: self.ui_controller.show_set_move_layout())
+      # buttons[4]       checkAble
+        buttons[5]      .clicked.connect(lambda: self.ui_controller.show_min_green_layout())
+      # buttons[6]       TODO
+        buttons[7]      .clicked.connect(lambda: self.ui_controller.show_matrix_layout())
+      # buttons[8]       checkAble
+        buttons[9]      .clicked.connect(lambda: self.ui_controller.show_sk_layout())
+      # buttons[10]      checkAble
+      # buttons[11]      TODO
+      # buttons[12]      TODO
+        buttons[13]     .clicked.connect(lambda: displayAllMoves())
 
         # create grid
-        grid.addWidget(name_edit, 0, 0, 1, 2) # row_num, col_num, how many rows use, how many columns to use
+        btn_grid.addWidget(name_edit, 0, 0, 1, 2) # row_num, col_num, how many rows use, how many columns to use
         for i, btn in enumerate(buttons, start=1):
             r = (i - 1) // 2 + 1
             c = (i - 1) % 2
-            grid.addWidget(btn, r, c)
+            btn_grid.addWidget(btn, r, c)
 
         # גלילה אם יש המון כפתורים
         scroll = QScrollArea(root)
@@ -63,7 +68,6 @@ class MainWindow(QMainWindow):
         scroll.setFixedWidth(400)  # תבחר רוחב שמתאים לכל הטקסט בכפתורים
 
         # צרף ללייאאוט הראשי
-        root_layout.addWidget(left_panel, 1)   # תן לפאנל השמאלי לגדול
         root_layout.addWidget(scroll, 0)  # עמודת כפתורים קבועה יחסית
 
         # =============== add layouts =============== #
@@ -71,17 +75,15 @@ class MainWindow(QMainWindow):
         root_layout.addWidget(self.ui_controller.get_min_green_layout(), 20)
         root_layout.addWidget(self.ui_controller.get_matrix_layout(), 20)
         root_layout.addWidget(self.ui_controller.get_sk_layout(), 20)
-        root_layout.addWidget(scroll, 2)  # במקום 0 – זה יתן לו פי 2 גמישות מהריבוע השמאלי
+        root_layout.addSpacerItem(QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum))
+        root_layout.addWidget(scroll)
 
-        grid.setContentsMargins(16, 16, 16, 16)
-        grid.setHorizontalSpacing(12)
-        grid.setVerticalSpacing(12)
-
-        # grid.setColumnStretch(0, 1)
-        # grid.setColumnStretch(1, 1)
+        btn_grid.setContentsMargins(16, 16, 16, 16)
+        btn_grid.setHorizontalSpacing(12)
+        btn_grid.setVerticalSpacing(12)
 
         rows = (len(buttons) + 1) // 2 + 1  # +1 בגלל השורה של name_edit
-        grid.setRowStretch(rows, 1)  # “רווח גמיש” אחרי השורה האחרונה
+        btn_grid.setRowStretch(rows, 1)  # “רווח גמיש” אחרי השורה האחרונה
         scroll.setWidgetResizable(True)
 
         set_btn_disable([buttons[0]] + buttons[2:])                                     # Disable buttons
